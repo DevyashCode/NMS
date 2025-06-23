216db7aa0c9fae00469b0fa51ae4467018100fbd
DevyashCode <raseladevyash@gmail.com>
2025-06-23 15:57:44 +0530

    Update AddNetwork.jsx

--- src/components/AddNetwork/AddNetwork.jsx
+++ src/components/AddNetwork/AddNetwork.jsx
@@ -1,4 +1,4 @@
-import Header from "../SectionHeader/header";
+import Header from "../SectionHeader/Header.jsx";
 import AddNetworkForm from "./AddNetworkForm";
 import SearchIpAddress from "./SearchIPAddress";
 
